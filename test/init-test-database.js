29b49a4c704e4a893d7f8617eff9ad7590f44eec
Rémi Bouchar <remi.bouchar@gmail.com>
2019-03-18 15:58:15 +0000

    fix after review

--- test/init-test-database.js
+++ test/init-test-database.js
@@ -1,12 +1,12 @@
 const knex = require('knex')({
   client: 'pg',
-  connection: process.env.DB_PASSWORD ? 'postgres://postgres:password@localhost:5432/mantle_auth_test' : 'postgres://postgres@localhost:5432/mantle_auth_test'
+  connection: process.env.DB_PASSWORD ? `postgres://postgres:${process.env.DB_PASSWORD}@localhost:5432/mantle_auth_test` : 'postgres://postgres@localhost:5432/mantle_auth_test'
 })
 
 const initDb = async () => {
   try {
-    await knex.raw('CREATE TABLE "person" ("name" VARCHAR, "email" VARCHAR NOT NULL PRIMARY KEY, "password" VARCHAR);')
-    process.exit(0)
+    await knex.raw('drop table if exists "person";CREATE TABLE "person" ("name" VARCHAR, "email" VARCHAR NOT NULL PRIMARY KEY, "password" VARCHAR);')
+    await knex.destroy()
   } catch (err) {
     console.error(err)
     process.exit(1)
