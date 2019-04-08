1d6bd722224729d5c58d83905eb838fa3cdea0dc
Dean Richer <dpbricher@gmail.com>
2019-04-08 15:36:51 +0100

    feat: created a class for mantle auth errors

---
+++ errors/base.js
@@ -0,0 +1,26 @@
+class MantleAuthError extends Error {
+  constructor({
+    fileName,
+    message,
+    lineNumber,
+    name,
+    expose = true,
+    status = 500
+  }) {
+    super(message, fileName, lineNumber)
+
+    this.expose = expose
+    this.name = name
+    this.status = status
+
+    this.isMantleAuth = true
+  }
+}
+
+MantleAuthError.ERRORS = {
+  ACCOUNT_LOCKED: 'AccountLocked',
+  INVALID_USER: 'InvalidUser',
+  INVALID_PASSWORD: 'InvalidPassword'
+}
+
+module.exports = MantleAuthError
